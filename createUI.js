d9bdbb7e563a8f0484b2e41f2e489002592ec7fd
Lucas <mamutthe2015@gmail.com>
2022-09-05 15:53:56 -0300

    projeto reescrito em TS

--- createUI.js
+++ createUI.js
@@ -1,201 +1,192 @@
-const _TYPELOC = ["leftNavbar", "rightNavbar"];
-
 /**
  *  Criar um elemento de um botão
  */
-class CreateButton {
-  /**
-   *
-   * @param {*} name - Nome de exibição do botão.
-   * @param {*} loc  - Localização do elemento na tela, ele vai adicionar uma class leftNavbar ou rightNavbar.
-   */
-  constructor(name, loc) {
-    this.name = name;
-    if (!_TYPELOC.includes(loc))
-      throw new Error(`${loc} não é um TypeLoc valido!`);
-
-    this.loc = document.querySelector(`.${loc}`);
-  }
-
-  /**
-   * Criar um elemento de imagem.
-   *
-   * @param {*} svgIcon - Nome do arquivo em /icons
-   */
-  set buttonIcon(svgIcon) {
-    let buttonIcon = document.createElement("img");
-    buttonIcon.setAttribute("class", "buttonIcon");
-    buttonIcon.setAttribute("src", `/icons/${svgIcon}`);
-
-    this._buttonIcon = buttonIcon;
-  }
-
-  /**
-   * Setar HTML da ação do botão.
-   *
-   * @param {*} action - HTML
-   */
-  set buttonAction(action) {
-    this._buttonAction = action;
-  }
-
-  /**
-   * Retornar o HTML da ação do botão.
-   */
-  get buttonAction() {
-    return this._buttonAction;
-  }
-
-  /**
-   *  Retornar o elemento da imagem gerada pela função buttonIcon.
-   */
-  get buttonIcon() {
-    return this._buttonIcon;
-  }
-
-  /**
-   * Gerar o HTML do botão.
-   */
-  get HTML() {
-    if (this.name === undefined || this.loc === undefined)
-      throw new Error("Invalid HTML");
-
-    const filterButton = document.createElement("button");
-    filterButton.setAttribute("type", "button");
-    filterButton.setAttribute("class", "filterButton purpleGradient");
-    filterButton.setAttribute("id", this.name);
-    filterButton.setAttribute("title", this.name);
-
-    // Adicionar Icone
-    if (this._buttonIcon !== undefined)
-      filterButton.appendChild(this.buttonIcon);
-
-    return filterButton;
-  }
-
-  /**
-   * Salvar o HTML do botão no elemento da localização escolhida.
-   */
-  save() {
-    const buttonEntry = document.createElement("button-entry");
-
-    if (this._buttonAction !== undefined) {
-      const buttonAction = document.createElement("button-action");
-      buttonAction.appendChild(this.buttonAction);
-      buttonEntry.appendChild(buttonAction);
+var CreateButton = /** @class */ (function () {
+    /**
+     *
+     * @param {*} name - Nome de exibição do botão.
+     * @param {*} loc  - Localização do elemento na tela, ele vai adicionar uma class leftNavbar ou rightNavbar.
+     */
+    function CreateButton(name, loc) {
+        this.name = name;
+        if (!_TYPELOC.includes(loc))
+            throw new Error("".concat(loc, " n\u00E3o \u00E9 um TypeLoc valido!"));
+        this.loc = document.querySelector(".".concat(loc));
     }
-
-    buttonEntry.appendChild(this.HTML);
-
-    this.loc.appendChild(buttonEntry);
-  }
-}
-
+    Object.defineProperty(CreateButton.prototype, "buttonIcon", {
+        /**
+         *  Retornar o elemento da imagem gerada pela função buttonIcon.
+         */
+        get: function () {
+            return this._buttonIcon;
+        },
+        /**
+         * Criar um elemento de imagem.
+         *
+         * @param {*} svgIcon - Nome do arquivo em /icons
+         */
+        set: function (svgIcon) {
+            var buttonIcon = document.createElement("img");
+            buttonIcon.setAttribute("class", "buttonIcon");
+            buttonIcon.setAttribute("src", "/icons/".concat(svgIcon));
+            this._buttonIcon = buttonIcon;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreateButton.prototype, "buttonAction", {
+        /**
+         * Retornar o HTML da ação do botão.
+         */
+        get: function () {
+            return this._buttonAction;
+        },
+        /**
+         * Setar HTML da ação do botão.
+         *
+         * @param {*} action - HTML
+         */
+        set: function (action) {
+            this._buttonAction = action;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreateButton.prototype, "HTML", {
+        /**
+         * Gerar o HTML do botão.
+         */
+        get: function () {
+            if (this.name === undefined || this.loc === undefined)
+                throw new Error("Invalid HTML");
+            var filterButton = document.createElement("button");
+            filterButton.setAttribute("type", "button");
+            filterButton.setAttribute("class", "filterButton purpleGradient");
+            filterButton.setAttribute("id", this.name);
+            filterButton.setAttribute("title", this.name);
+            // Adicionar Icone
+            if (this._buttonIcon !== undefined)
+                filterButton.appendChild(this.buttonIcon);
+            return filterButton;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    /**
+     * Salvar o HTML do botão no elemento da localização escolhida.
+     */
+    CreateButton.prototype.save = function () {
+        var buttonEntry = document.createElement("button-entry");
+        if (this._buttonAction !== undefined) {
+            var buttonAction = document.createElement("button-action");
+            buttonAction.appendChild(this.buttonAction);
+            buttonEntry.appendChild(buttonAction);
+        }
+        buttonEntry.appendChild(this.HTML);
+        this.loc.appendChild(buttonEntry);
+    };
+    return CreateButton;
+}());
 /**
  *  Modulos da página
  */
-class Start {
-  /**
-   *  Botões
-   */
-  loadingButtons() {
-    const listButtons = [
-      {
-        name: "Brightness",
-        icon: "brightness.svg",
-        pos: "leftNavbar",
-        action: () => {
-          const inputRange = document.createElement("input");
-          inputRange.setAttribute("type", "range");
-          inputRange.setAttribute("class", "range");
-          inputRange.setAttribute("min", "0.0");
-          inputRange.setAttribute("max", "8");
-          inputRange.setAttribute("value", "0");
-          inputRange.setAttribute("step", "0.1");
-
-          return inputRange;
-        },
-      },
-      {
-        name: "Contrast",
-        icon: "contrast.svg",
-        pos: "leftNavbar",
-        action: undefined,
-      },
-      {
-        name: "Sharpness",
-        icon: "sharpness.svg",
-        pos: "leftNavbar",
-        action: undefined,
-      },
-      {
-        name: "Temperature",
-        icon: "temperature.svg",
-        pos: "rightNavbar",
-        action: undefined,
-      },
-      {
-        name: "Color",
-        icon: "color.svg",
-        pos: "rightNavbar",
-        action: undefined,
-      },
-      {
-        name: "Information",
-        icon: "information.svg",
-        pos: "rightNavbar",
-        action: undefined,
-      },
-    ];
-
-    listButtons.forEach((buttonObj) => {
-      const { name, icon, pos, action } = buttonObj;
-      const button = new CreateButton(name, pos);
-      button.buttonIcon = icon;
-      if (action !== undefined) button.buttonAction = action();
-      button.save();
-    });
-  }
-
-  /**
-   *  Um Footer???
-   */
-  footer() {
-    const footer = document.createElement("figcaption");
-    footer.setAttribute("class", "footer purpleBackgound");
-    document.body.appendChild(footer);
-  }
-
-  /**
-   * Evento do input[type=range]
-   */
-  eventRange() {
-    const range = document.querySelector(".range");
-    const style = document.createElement("style");
-    document.body.appendChild(style);
-
-    range.addEventListener("input", () => {
-      const value = range.value;
-      const valueBar = (100 * value) / range.max;
-      style.textContent = `.range {background: linear-gradient(to right, #1976d2 ${valueBar}%, #a8cbee ${valueBar}%);}`;
-      style.textContent += `.currentImage {filter: brightness(${Math.abs(
-        value > range.max / 1.5 ? value : value / 4
-      )});}`;
-    });
-  }
-}
-
+var Start = /** @class */ (function () {
+    function Start() {
+    }
+    /**
+     *  Botões
+     */
+    Start.prototype.loadingButtons = function () {
+        var listButtons = [
+            {
+                name: "Brightness",
+                icon: "brightness.svg",
+                pos: "leftNavbar",
+                action: function () {
+                    var inputRange = document.createElement("input");
+                    inputRange.setAttribute("type", "range");
+                    inputRange.setAttribute("class", "range");
+                    inputRange.setAttribute("min", "0.0");
+                    inputRange.setAttribute("max", "8");
+                    inputRange.setAttribute("value", "0");
+                    inputRange.setAttribute("step", "0.1");
+                    return inputRange;
+                }
+            },
+            {
+                name: "Contrast",
+                icon: "contrast.svg",
+                pos: "leftNavbar",
+                action: undefined
+            },
+            {
+                name: "Sharpness",
+                icon: "sharpness.svg",
+                pos: "leftNavbar",
+                action: undefined
+            },
+            {
+                name: "Temperature",
+                icon: "temperature.svg",
+                pos: "rightNavbar",
+                action: undefined
+            },
+            {
+                name: "Color",
+                icon: "color.svg",
+                pos: "rightNavbar",
+                action: undefined
+            },
+            {
+                name: "Information",
+                icon: "information.svg",
+                pos: "rightNavbar",
+                action: undefined
+            },
+        ];
+        listButtons.forEach(function (buttonObj) {
+            var name = buttonObj.name, icon = buttonObj.icon, pos = buttonObj.pos, action = buttonObj.action;
+            var button = new CreateButton(name, pos);
+            button.buttonIcon = icon;
+            if (action !== undefined)
+                button.buttonAction = action();
+            button.save();
+        });
+    };
+    /**
+     *  Um Footer???
+     */
+    Start.prototype.footer = function () {
+        var footer = document.createElement("figcaption");
+        footer.setAttribute("class", "footer purpleBackgound");
+        document.body.appendChild(footer);
+    };
+    /**
+     * Evento do input[type=range]
+     */
+    Start.prototype.eventRange = function () {
+        var range = document.querySelector(".range");
+        var style = document.createElement("style");
+        document.body.appendChild(style);
+        range.addEventListener("input", function () {
+            var value = range.value;
+            var valueBar = (100 * value) / range.max;
+            style.textContent = ".range {background: linear-gradient(to right, #1976d2 ".concat(valueBar, "%, #a8cbee ").concat(valueBar, "%);}");
+            style.textContent += ".currentImage {filter: brightness(".concat(Math.abs(value > range.max / 1.5 ? value : value / 4), ");}");
+        });
+    };
+    return Start;
+}());
 // Carregar modulos
 (function () {
-  const loadingPage = new Start();
-  loadingPage.loadingButtons();
-  loadingPage.footer();
-  loadingPage.eventRange();
+    var loadingPage = new Start();
+    loadingPage.loadingButtons();
+    loadingPage.footer();
+    loadingPage.eventRange();
 })();
-
-const obj = {
-  x: 0,
-  set addX(value) {
-    this.x = value + 1;
-  },
+var obj = {
+    x: 0,
+    set addX(value) {
+        this.x = value + 1;
+    }
 };
---
+++ createUI.ts
@@ -0,0 +1,262 @@
+type positionType = 'leftNavbar' | 'rightNavbar';
+
+class Button {
+  name: string;
+  position: positionType;
+  private $imgElement: HTMLImageElement | undefined;
+  private action: unknown;
+
+  constructor(name: string, position: positionType) {
+    this.name = name;
+    this.position = position;
+    this.$imgElement = undefined;
+    this.action = undefined;
+  }
+
+  setIcon(name: `${string}.svg`): void {
+    const $getElementImg = document.createElement('img');
+    $getElementImg.setAttribute('class', 'buttonIcon');
+    $getElementImg.setAttribute('src', `/icons/${name}`);
+    this.$imgElement = $getElementImg;
+  }
+
+  setAction(action: unknown): void {
+    this.action = action;
+  }
+
+  private get HTML(): HTMLButtonElement {
+    const $filterButton = document.createElement('button');
+    $filterButton.setAttribute('type', 'button');
+    $filterButton.setAttribute('class', 'filterButton purpleGradient');
+    $filterButton.setAttribute('id', this.name);
+    $filterButton.setAttribute('title', this.name);
+
+    if (this.$imgElement !== undefined) {
+      $filterButton.appendChild(this.$imgElement);
+    }
+
+    return $filterButton;
+  }
+
+  create(): void {
+    const $buttonEntry = document.createElement('button-entry');
+    const $locationElement = document.querySelector(`.${this.position}`);
+
+    if ($locationElement === null) {
+      throw new Error('Location Element is null, what?');
+    }
+
+    if (this.action !== undefined) {
+      const $buttonAction = document.createElement('button-action');
+      $buttonAction.appendChild(this.action as Node);
+      $buttonEntry.appendChild($buttonAction);
+    }
+
+    $buttonEntry.appendChild(this.HTML);
+
+    $locationElement.appendChild($buttonEntry);
+  }
+}
+
+/* type posType = 'leftNavbar' | 'rightNavbar';
+ type buttonListType = Array<{
+  name: string;
+  icon: string;
+  pos: posType;
+  action: () => void;
+}>;
+
+class CreateButton {
+  /**
+   *
+   * @param {*} name - Nome de exibição do botão.D
+   * @param {*} loc  - Localização do elemento na tela, ele vai adicionar uma class leftNavbar ou rightNavbar.
+  name: string;
+  pos: Element | null;
+  $icon: Element | null;
+  _action: unknown;
+  constructor(name: string, pos: posType) {
+    this.name = name;
+    this.pos = document.querySelector(`.${pos}`);
+    this.$icon = null;
+    this._action = undefined;
+  }
+
+  set buttonIcon(icon: string) {
+    const $elementImg = document.createElement('img');
+    $elementImg.setAttribute('class', 'buttonIcon');
+    $elementImg.setAttribute('src', `/icons/${icon}`);
+    this.$icon = $elementImg;
+  }
+
+  /**
+   * Setar HTML da ação do botão.
+   *
+   * @param {*} action - HTML
+
+  set buttonAction(action) {
+    this._action = action;
+  }
+
+  /**
+
+  get buttonAction(): unknown {
+    return this._action;
+  }
+
+  /**
+   *  Retornar o elemento da imagem gerada pela função buttonIcon.
+
+  get buttonIcon() {
+    return this.$icon;
+  }
+
+  /**
+   * Gerar o HTML do botão.
+
+  get HTML() {
+    if (this.name === undefined || this.loc === undefined) {
+      throw new Error('Invalid HTML');
+    }
+
+    const filterButton = document.createElement('button');
+    filterButton.setAttribute('type', 'button');
+    filterButton.setAttribute('class', 'filterButton purpleGradient');
+    filterButton.setAttribute('id', this.name);
+    filterButton.setAttribute('title', this.name);
+
+    // Adicionar Icone
+    if (this._buttonIcon !== undefined) {
+      filterButton.appendChild(this.buttonIcon);
+    }
+
+    return filterButton;
+  }
+
+  /**
+   * Salvar o HTML do botão no elemento da localização escolhida.
+
+  save() {
+    const buttonEntry = document.createElement('button-entry');
+
+    if (this.buttonAction !== undefined) {
+      const buttonAction = document.createElement('button-action');
+      buttonAction.appendChild(this.buttonAction);
+      buttonEntry.appendChild(buttonAction);
+    }
+
+    buttonEntry.appendChild(this.HTML);
+
+    this.loc.appendChild(buttonEntry);
+  }
+}
+
+/**
+ *  Modulos da página
+
+class Start {
+  /**
+   *  Botões
+
+  loadingButtons() {
+    const buttonList: buttonListType = [
+      {
+        name: 'Brightness',
+        icon: 'brightness.svg',
+        pos: 'leftNavbar',
+        action: () => {
+          const inputRange = document.createElement('input');
+          inputRange.setAttribute('type', 'range');
+          inputRange.setAttribute('class', 'range');
+          inputRange.setAttribute('min', '0.0');
+          inputRange.setAttribute('max', '8');
+          inputRange.setAttribute('value', '0');
+          inputRange.setAttribute('step', '0.1');
+
+          return inputRange;
+        },
+      },
+      {
+        name: 'Contrast',
+        icon: 'contrast.svg',
+        pos: 'leftNavbar',
+        action: undefined,
+      },
+      {
+        name: 'Sharpness',
+        icon: 'sharpness.svg',
+        pos: 'leftNavbar',
+        action: undefined,
+      },
+      {
+        name: 'Temperature',
+        icon: 'temperature.svg',
+        pos: 'rightNavbar',
+        action: undefined,
+      },
+      {
+        name: 'Color',
+        icon: 'color.svg',
+        pos: 'rightNavbar',
+        action: undefined,
+      },
+      {
+        name: 'Information',
+        icon: 'information.svg',
+        pos: 'rightNavbar',
+        action: undefined,
+      },
+    ];
+
+    buttonList.forEach((buttonPropertiesObj) => {
+      const { name, icon, pos, action } = buttonPropertiesObj;
+      const $button = new CreateButton(name, pos);
+      $button.buttonIcon = icon;
+      if (action !== undefined) $button.buttonAction = action();
+      $button.save();
+    });
+  }
+
+  /**
+   *  Um Footer???
+
+  footer() {
+    const footer = document.createElement('figcaption');
+    footer.setAttribute('class', 'footer purpleBackgound');
+    document.body.appendChild(footer);
+  }
+
+  /**
+   * Evento do input[type=range]
+
+  eventRange() {
+    const range = document.querySelector('.range');
+    const style = document.createElement('style');
+    document.body.appendChild(style);
+
+    range.addEventListener('input', () => {
+      const value = range.value;
+      const valueBar = (100 * value) / range.max;
+      style.textContent = `.range {background: linear-gradient(to right, #1976d2 ${valueBar}%, #a8cbee ${valueBar}%);}`;
+      style.textContent += `.currentImage {filter: brightness(${Math.abs(
+        value > range.max / 1.5 ? value : value / 4
+      )});}`;
+    });
+  }
+}
+
+// Carregar modulos
+(function () {
+  const loadingPage = new Start();
+  loadingPage.loadingButtons();
+  loadingPage.footer();
+  loadingPage.eventRange();
+})();
+
+const obj = {
+  x: 0,
+  set addX(value) {
+    this.x = value + 1;
+  },
+};
+*/
